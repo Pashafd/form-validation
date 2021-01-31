30b821e61517748fd4d464a78adf3da3c8eb09e2
Pasha <bayntis@gmail.com>
2021-01-31 19:37:48 +0200

    finish fith html start css

--- src/index.js
+++ src/index.js
@@ -1,15 +1,2 @@
 // import style
 import './style/index.scss';
-
-const developer = true;
-console.log(developer);
-    
-const obj = {
-  a: 'ss',
-  b: 'ssssss',
-};
-
-console.log(['a', 'b', 'c'].includes('b'));
-
-console.log(obj);
-console.log('ssadsad');
