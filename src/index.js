3ed065b19ec3a848b5a2a95ee199de8941a43bc8
Pasha <bayntis@gmail.com>
2021-01-31 18:55:16 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// import style
+import './style/index.scss';
+
+const developer = true;
+console.log(developer);
+    
+const obj = {
+  a: 'ss',
+  b: 'ssssss',
+};
+
+console.log(['a', 'b', 'c'].includes('b'));
+
+console.log(obj);
+console.log('ssadsad');
---
+++ webpack.config.js
@@ -0,0 +1,153 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode =
+  process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
+if (mode === 'production') {
+  module.exports = {
+    mode: mode,
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      port: 8080,
+      open: true,
+      compress: true,
+      hot: true,
+    },
+
+    entry: {
+      main: path.resolve(__dirname, './src/index.js'),
+    },
+
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'bundle.js',
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Form Validation',
+        template: path.resolve(__dirname, './src/template.html'),
+        filename: 'index.html',
+      }),
+
+      new CleanWebpackPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+      new MiniCssExtractPlugin(),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'src/assets/images'),
+            to: path.resolve(__dirname, 'dist', 'assets/images'),
+          },
+        ],
+      }),
+    ],
+
+    module: {
+      rules: [
+        //JS
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+
+        //img
+        {
+          test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+          type: 'asset/resource',
+        },
+
+        {
+          test: /\.(scss|css)$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 2,
+                sourceMap: false,
+              },
+            },
+            'postcss-loader',
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+
+    devtool: 'source-map',
+  };
+} else {
+  module.exports = {
+    mode: mode,
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      port: 8080,
+      open: true,
+      compress: true,
+      hot: true,
+    },
+
+    entry: {
+      main: path.resolve(__dirname, './src/index.js'),
+    },
+
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'bundle.js',
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Form Validation',
+        template: path.resolve(__dirname, './src/template.html'),
+        filename: 'index.html',
+      }),
+
+      new CleanWebpackPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+      new MiniCssExtractPlugin(),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'src/assets/images'),
+            to: path.resolve(__dirname, 'dist', 'assets/images'),
+          },
+        ],
+      }),
+    ],
+
+    module: {
+      rules: [
+        //JS
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+
+        //img
+        {
+          test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+          type: 'asset/resource',
+        },
+
+        // CSS, PostCSS, Sass
+        {
+          test: /\.(scss|css)$/,
+          use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+        },
+      ],
+    },
+  };
+}
