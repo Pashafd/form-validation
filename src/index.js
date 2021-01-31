7876efb0ebb125e14c19f2ea74ceeb85f1df58b1
Pasha <bayntis@gmail.com>
2021-01-31 20:30:26 +0200

    finish with main in css, go to js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 // import style
 import './style/index.scss';
+
+const form = document.querySelector('#form');
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const password = document.querySelector('#pasword');
+const paswordTwo = document.querySelector('#passwordTwo');
